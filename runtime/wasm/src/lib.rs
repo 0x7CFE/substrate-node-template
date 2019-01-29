3cace89336aa9cbc2de41a42a327675b0cf9ce33
Sergei Pepyakin <s.pepyakin@gmail.com>
2019-01-29 13:59:41 +0100

    Update lib.rs

--- runtime/wasm/src/lib.rs
+++ runtime/wasm/src/lib.rs
@@ -14,7 +14,7 @@
  // You should have received a copy of the GNU General Public License
 // along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
 
-// ! The Substrate runtime reexported for WebAssembly compile.
+//! The Substrate runtime reexported for WebAssembly compile.
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
